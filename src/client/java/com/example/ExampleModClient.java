64426d906e41bfcebe2a1650d266873429180e06
cenyaegorov <cv.egorova3815@gmail.com>
2025-11-16 00:30:33 +0300

    start

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -6,6 +6,5 @@ public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-
 	}
 }
